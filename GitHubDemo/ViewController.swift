bf443807da9c753697a244fed3b8fe92308dbda8
Huy Huynh <huyhuynh@Huys-MacBook-Pro.local>
2016-12-24 18:00:03 +0700

    Initial Commit

---
+++ GitHubDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitHubDemo
+//
+//  Created by Huy Huynh on 12/24/16.
+//  Copyright © 2016 Huy Huynh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
